da1d0a8e4bb90d780b58c0f208f4096236ba5028
wsspad <wsspad@gmail.com>
2016-07-12 21:29:50 +0200

    Pockets - small fix

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -7,6 +7,7 @@ include("sh_world.lua")
 include("sh_spaceship.lua")
 include("sh_write_stars_to_sql.lua")
 include("sh_map.lua")
+include("sh_pockets.lua")
 
 if World then
 	World.removeEverything()
