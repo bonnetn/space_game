6d53ce4f8722feae11a9126ad42087677dd63c55
wsspad <wsspad@gmail.com>
2016-07-12 22:55:46 +0200

    Fix to the load order - files are sent to clients now.

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -2,6 +2,13 @@ AddCSLuaFile()
 
 GrandEspace = {}
 
+AddCSLuaFile("sh_world.lua")
+AddCSLuaFile("sh_player_functions.lua")
+AddCSLuaFile("sh_spaceship.lua")
+AddCSLuaFile("sh_write_stars_to_sql.lua")
+AddCSLuaFile("sh_map.lua")
+AddCSLuaFile("sh_pockets.lua")
+
 include("sh_world.lua")
 include("sh_player_functions.lua")
 include("sh_spaceship.lua")
