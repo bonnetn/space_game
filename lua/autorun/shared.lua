a6cfaa0109bd32e41865fbb788582610263d1d2b
wsspad <wsspad@gmail.com>
2016-07-12 22:46:52 +0200

    Fix to load order. shared.lua

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -1,9 +1,9 @@
 AddCSLuaFile()
 
-include("sh_player_functions.lua")
 GrandEspace = {}
 
 include("sh_world.lua")
+include("sh_player_functions.lua")
 include("sh_spaceship.lua")
 include("sh_write_stars_to_sql.lua")
 include("sh_map.lua")
