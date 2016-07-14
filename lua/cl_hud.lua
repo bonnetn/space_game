65d2f7a196b672c7fa087d85948a5c1c8b0e52cf
wsspad <wsspad@gmail.com>
2016-07-14 15:44:45 +0200

    HUD now shows distance.

--- lua/cl_hud.lua
+++ lua/cl_hud.lua
@@ -7,8 +7,10 @@ hook.Add( "HUDPaint", "Grand_Espace - HUD while in space", function()
 	
 	for k, v in pairs( World.spaceships ) do
 		if ship ~= v then
-			local pos = ( ship:getPocketPos() + ( v:getGridPos() - ship:getGridPos() ) ):ToScreen()
+			local dist = v:getGridPos() - ship:getGridPos()
+			local pos = ( ship:getPocketPos() + dist ):ToScreen()
 			draw.SimpleText( "Spaceship " .. tostring( k ), "TargetID", pos.x + 16, pos.y - 16, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+			draw.SimpleText( "dist: " .. tostring( math.Round( dist:Length() ) ) .. "gu", "TargetID", pos.x + 16, pos.y + 4, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
 			surface.DrawCircle( pos.x, pos.y, 8, 255, 255, 255, 100 )
 		end
 	end
