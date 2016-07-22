ed255ea7e11c739822e704ff87aaa494b91cfdb6
wsspad <wsspad@gmail.com>
2016-07-22 14:08:01 +0200

    Third Person works again.

--- lua/entities/pilot_interface.lua
+++ lua/entities/pilot_interface.lua
@@ -40,104 +40,102 @@ function ENT:Think()
 	if CLIENT then
 		local ply = LocalPlayer()
 	
-		if not ply:InVehicle() then
+		if not ply:InVehicle() and GrandEspace.getThirdPerson() then
 			GrandEspace.setThirdPerson( false )
-		return true end
+		else
+			if IsValid( self.seat ) and self.seat:IsVehicle() then
+				if ply:GetVehicle() == self.seat then
+					GrandEspace.setThirdPerson( self.seat:GetThirdPersonMode() )
+				end	
+			end
+		end
+	else
+		if not self.parentSpaceship then return true end
+		if not self.Inputs then return true end
 		
-		if ( not IsValid( self.seat ) ) or ( not self.seat:IsVehicle() ) then return true end
-		if ply:GetVehicle() ~= self.seat then return true end
+		local ship = self.parentSpaceship
 		
-		GrandEspace.setThirdPerson( self.seat:GetThirdPersonMode() )
-	return true end
-	
-	if not self.parentSpaceship or not self.parentSpaceship.getGridAngle then return true end
-	if not self.Inputs then return true end
-	
-	local ship = self.parentSpaceship
-	
-	// This will be later implemented into spaceship class and depend on modules or other shit.
-	local speed = 1000
-	local degrees = 50
-	
-	local velocity = ship:getVelocity()
-	local angVelocity = ship:getAngularVelocity()
-	
-	local gridAngle = ship:getGridAngle()
-	
-	local acceleration = Vector()
-	local angularAcceleration = Angle()
-	
-	// Moving
-	if self:GetWireInputAsNumber( "Forward" ) > 0 then
-		acceleration = acceleration + gridAngle:Forward() * speed
-	elseif self:GetWireInputAsNumber( "Backward" ) > 0 then
-		acceleration = acceleration - gridAngle:Forward() * speed
-	end
-	
-	if self:GetWireInputAsNumber( "Right" ) > 0 then
-		acceleration = acceleration + gridAngle:Right() * speed
-	elseif self:GetWireInputAsNumber( "Left" ) > 0 then
-		acceleration = acceleration - gridAngle:Right() * speed
-	end
-	
-	if self:GetWireInputAsNumber( "Up" ) > 0 then
-		acceleration = acceleration + gridAngle:Up() * speed
-	elseif self:GetWireInputAsNumber( "Down" ) > 0 then
-		acceleration = acceleration - gridAngle:Up() * speed
-	end
-	
-	// Turning
-	if self:GetWireInputAsNumber( "PitchUp" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(-degrees, 0, 0)
-	elseif self:GetWireInputAsNumber( "PitchDown" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(degrees, 0, 0)
-	end
-	
-	if self:GetWireInputAsNumber( "YawLeft" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(0, degrees, 0)
-	elseif self:GetWireInputAsNumber( "YawRight" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(0, -degrees, 0)
-	end
-	
-	if self:GetWireInputAsNumber( "RollRight" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(0, 0, degrees)
-	elseif self:GetWireInputAsNumber( "RollLeft" ) > 0 then
-		angularAcceleration = angularAcceleration + Angle(0, 0, -degrees)
-	end
-	
-	// Drag
-	if acceleration.x == 0 then
-		acceleration.x = -velocity.x
-	end
-	
-	if acceleration.y == 0 then
-		acceleration.y = -velocity.y
-	end
-	
-	if acceleration.z == 0 then
-		acceleration.z = -velocity.z
-	end
-	
-	if angularAcceleration.x == 0 then
-		angularAcceleration.x = -angVelocity.x
-	end
-	
-	if angularAcceleration.y == 0 then
-		angularAcceleration.y = -angVelocity.y
-	end
-	
-	if angularAcceleration.z == 0 then
-		angularAcceleration.z = -angVelocity.z
-	end
-	
-	ship:setAcceleration( acceleration, true )
-	ship:setAngularAcceleration( angularAcceleration, true )
-	
-	if not self.Inputs[ "Seat" ] then
-		if not self.Inputs[ "Seat" ].value then
-			self.seat = self.Inputs[ "Seat" ].value
-			self:SendToClients()
-		end		
+		// This will be later implemented into spaceship class and depend on modules or other shit.
+		local speed = 1000
+		local degrees = 50
+		
+		local velocity = ship:getVelocity()
+		local angVelocity = ship:getAngularVelocity()
+		
+		local gridAngle = ship:getGridAngle()
+		
+		local acceleration = Vector()
+		local angularAcceleration = Angle()
+		
+		// Moving
+		if self:GetWireInputAsNumber( "Forward" ) > 0 then
+			acceleration = acceleration + gridAngle:Forward() * speed
+		elseif self:GetWireInputAsNumber( "Backward" ) > 0 then
+			acceleration = acceleration - gridAngle:Forward() * speed
+		end
+		
+		if self:GetWireInputAsNumber( "Right" ) > 0 then
+			acceleration = acceleration + gridAngle:Right() * speed
+		elseif self:GetWireInputAsNumber( "Left" ) > 0 then
+			acceleration = acceleration - gridAngle:Right() * speed
+		end
+		
+		if self:GetWireInputAsNumber( "Up" ) > 0 then
+			acceleration = acceleration + gridAngle:Up() * speed
+		elseif self:GetWireInputAsNumber( "Down" ) > 0 then
+			acceleration = acceleration - gridAngle:Up() * speed
+		end
+		
+		// Turning
+		if self:GetWireInputAsNumber( "PitchUp" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(-degrees, 0, 0)
+		elseif self:GetWireInputAsNumber( "PitchDown" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(degrees, 0, 0)
+		end
+		
+		if self:GetWireInputAsNumber( "YawLeft" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(0, degrees, 0)
+		elseif self:GetWireInputAsNumber( "YawRight" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(0, -degrees, 0)
+		end
+		
+		if self:GetWireInputAsNumber( "RollRight" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(0, 0, degrees)
+		elseif self:GetWireInputAsNumber( "RollLeft" ) > 0 then
+			angularAcceleration = angularAcceleration + Angle(0, 0, -degrees)
+		end
+		
+		// Drag
+		if acceleration.x == 0 then
+			acceleration.x = -velocity.x
+		end
+		
+		if acceleration.y == 0 then
+			acceleration.y = -velocity.y
+		end
+		
+		if acceleration.z == 0 then
+			acceleration.z = -velocity.z
+		end
+		
+		if angularAcceleration.x == 0 then
+			angularAcceleration.x = -angVelocity.x
+		end
+		
+		if angularAcceleration.y == 0 then
+			angularAcceleration.y = -angVelocity.y
+		end
+		
+		if angularAcceleration.z == 0 then
+			angularAcceleration.z = -angVelocity.z
+		end
+		
+		ship:setAcceleration( acceleration, true )
+		ship:setAngularAcceleration( angularAcceleration, true )
+		
+		if self.Inputs[ "Seat" ] and self.Inputs[ "Seat" ].value then
+			self:SendToClients( self.Inputs[ "Seat" ].value )	
+		end
 	end
 	
 	return true
@@ -159,12 +157,12 @@ function ENT:TriggerInput( iname, value )
 	self.Inputs[ iname ].value = value
 end
 
-function ENT:SendToClients()
+function ENT:SendToClients( seat )
 	if CLIENT then return end
 	
 	net.Start( "PulpMod_PilotInterface" )
 		net.WriteInt( self:EntIndex(), 32 )
-		net.WriteInt( self.seat:EntIndex(), 32 )
+		net.WriteInt( seat:EntIndex(), 32 )
 	net.Broadcast()
 end
 
