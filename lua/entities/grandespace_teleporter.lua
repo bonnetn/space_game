74420495d0b3255764e1bf1205e1337cdfd86c10
wsspad <wsspad@gmail.com>
2016-07-31 09:28:22 +0200

    Doctor's back in business.

--- lua/entities/grandespace_teleporter.lua
+++ lua/entities/grandespace_teleporter.lua
@@ -7,13 +7,15 @@ ENT.Type = "anim"
 ENT.Base = "base_anim"
 
 function ENT:Initialize()
-	if CLIENT then return end
+	if CLIENT then
+		self:SetRenderBounds( Vector( -60, -60, -20 ), Vector( 60, 60, 120 ) )
+	return end
 	
 	self:SetModel( "models/mechanics/wheels/wheel_speed_72.mdl" )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
 	self:PhysicsInit( SOLID_VPHYSICS )
-
+	
 	local phys = self:GetPhysicsObject()
 	if IsValid( phys ) then
 		phys:Wake()
@@ -28,8 +30,39 @@ function ENT:Think()
 	end
 end
 
+if CLIENT then
+	-- A font that will be used on the control panel
+	surface.CreateFont( "PanelFont", { font = "DermaLarge", size = 96, weight = 500 } )
+end
+
+-- pos, size, label and onClick
+local function drawButton( x, y, w, h, t, f )
+	draw.RoundedBox( 0, x, y, w, h, Color( 255, 255, 255 ) )
+	draw.SimpleText( t, "PanelFont", x + w/2, y + h/2, Color( 0, 0, 0 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+end
+
+local function drawCursor()
+	local pos, rot = EyePos(), EyeAngles()
+	local x, y = 0, 0
+	-- Some highly advanced magical equations will go here.
+	draw.RoundedBox( 0, x - 4, y - 4, 8, 8, Color( 255, 255, 255 ) )
+end
+
+-- I need to draw a quad using render.DrawQuad() and not 3d2d camera because of depth buffer issues.
+local function drawPanel( pos, rotation )
+	render.SetColorMaterial()
+	render.DrawQuadEasy( pos, rotation:Forward(), 40, 40, Color( 0, 0, 0, 255 ), 0 )
+	
+	-- Now I need to draw the GUI. I wonder how can I make the button without using hacky sphagetti code...
+	cam.Start3D2D( pos + rotation:Right() * 20 + rotation:Up() * 20, Angle( rotation.x, rotation.y + 90, rotation.z + 90 ), 0.025 )
+		drawButton( 80, 80, 500, 100, "BUTTON", nil )
+		drawCursor()
+	cam.End3D2D()
+end
+
 function ENT:Draw()
 	self:DrawModel()
+	drawPanel( self:GetPos() + self:GetUp() * 50 + self:GetForward() * 40, self:GetAngles() )
 end
 
 function ENT:RetrievePlayer( ply )
